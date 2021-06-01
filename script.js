eebc8c96cd629f63c1afda4536c74825ab562718
Kermoren <wowadaswow@gmail.com>
2021-06-01 23:55:13 +0100

    Working timer, text and background need some adjustment

---
+++ script.js
@@ -0,0 +1,23 @@
+const daysEl = document.getElementById("days");
+const hoursEl = document.getElementById("hours");
+const minEl = document.getElementById("min");
+const secondsEl = document.getElementById("seconds");
+
+const fionasParty = "12 June 2021";
+function countdown (){
+    const fionaPartyDate = new Date(fionasParty)
+let currentDate = new Date ()
+let timeLeft =Math.floor((fionaPartyDate-currentDate) / 1000)
+console.log(timeLeft)
+let seconds = Math.floor(timeLeft%60)
+let min = Math.floor(timeLeft/60%60)
+let hours = Math.floor(timeLeft/3600%60)
+let days= Math.floor(timeLeft / 3600 / 24)
+console.log(days, hours, min)
+daysEl.innerHTML= days
+hoursEl.innerHTML= hours
+minEl.innerHTML= min
+secondsEl.innerHTML= seconds
+}
+countdown()
+setInterval(countdown, 1000)
